968b3eb414c2d295cf60856d240afaff39c24568
Maxi-M <maxim.moskovskiy@mail.ru>
2020-06-15 21:30:44 +0300

    Lesson 1

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const messagesData = ["Сообщение 1", "Сообщение 2"];
+
+const Message = (props) => <div>{props.text}</div>
+
+const MessageList = ({data}) => {
+    return data.map((item, index) => <Message text={item} key={index} />);
+}
+
+// const MessageInput = (props) => <input type="text" value={props.value}/>
+
+// const handleClick = () => {
+//     messagesData.push(MessageInput.text);
+// }
+// const ButtonSend = () => {
+//     return <div onClick={handleClick}>Send</div>
+// }
+
+ReactDom.render(
+    <MessageList data={messagesData} />,
+//    <MessageInput value="П" />,
+//    <ButtonSend />,
+    document.getElementById('root'),
+);
